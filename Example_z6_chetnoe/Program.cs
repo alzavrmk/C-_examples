e7c4366143cadc56df2a25c35eedf50fa97761c8
Anastasia <3845303@gmail.com>
2022-11-12 23:03:00 +0900

    четное или нечетное число

---
+++ Example_z6_chetnoe/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("a= ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 ==0 )
+{
+   Console.Write($"Число {numberA} четное");
+}
+else
+{
+  Console.Write($"Число {numberA} нечетное");
+}
