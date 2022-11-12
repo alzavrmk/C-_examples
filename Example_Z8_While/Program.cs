8d49ee0bcdc6c10f88606f0887b011d6628a865b
Anastasia <3845303@gmail.com>
2022-11-12 23:27:03 +0900

    Ряд четных чисел

---
+++ Example_Z8_While/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("N= ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int counte = 2;
+while (counte < numberN)
+{
+    //Console.Write($"{counte} ");
+    counte = counte +2;
+}
