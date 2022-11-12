5da287d02ed2d12ad6ff809e7040fa09a23f21f2
Anastasia <3845303@gmail.com>
2022-11-12 22:25:41 +0900

    max из двух чисел

---
+++ Example_z2_if/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("a= ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b= ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (numberA > numberB)
+{
+   max = numberA;
+}
+else
+{
+  max = numberB;  
+}
+Console.Write("max=");
+Console.Write(max);
