011c12e1c667acecf69f9f64f8b408800d5f16b1
Anastasia <3845303@gmail.com>
2022-11-12 22:52:12 +0900

    Максимальное из трез чисел

---
+++ Example_z4_if/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("a= ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b= ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("c= ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
+{
+   max = numberB;
+}
+else if (numberC>max)
+{
+  max = numberC;  
+}
+Console.Write($"max={max}");
