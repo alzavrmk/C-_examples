3044182bfe2535057474d4bb24321168a0d26f61
Anastasia <3845303@gmail.com>
2022-11-13 19:20:32 +0900

    Создала метод на C#

---
+++ Example005_METOD_MAX/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 5;
+int b1 = 48;
+int c1 = 47;
+
+int a2 = 522;
+int b2 = 4833;
+int c2 = 472;
+
+int a3 = 50;
+int b3 = 418;
+int c3 = 474;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+Console.WriteLine(max);
