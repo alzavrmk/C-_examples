db337b99c16908de9db2b2d004743904b5f5c136
Anastasia <3845303@gmail.com>
2022-11-15 07:07:52 +0900

    Задача 15. Определить является ли день выходным. Вводится номер дня недели

---
+++ Example016_DZ15/Program.cs
@@ -0,0 +1,6 @@
+﻿// По номеру для недели определить является ли он выходным.
+Console.Write ("Введите номер дня недели (число от 1 до 7) ");
+int dayWeek = Convert.ToByte(Console.ReadLine());
+
+if (dayWeek<6) Console.WriteLine ("Нет");
+else Console.WriteLine("Да, выходной");
