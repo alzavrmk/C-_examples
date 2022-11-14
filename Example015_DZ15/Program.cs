a1b356223c3afab90e2876026fc9845287d19ca2
Anastasia <3845303@gmail.com>
2022-11-15 06:54:30 +0900

    Задача 13.Программа выводит третью цифру справа в любом многозначном числе если эта цифра существует.

---
+++ Example015_DZ15/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа выводит третью цифру справа в любом многозначном числе если эта цифра существует.
+Console.Write("Введите любое целое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if (numA < 100) Console.WriteLine("Третьей цифры в этом числе нет");
+else
+{
+    while (numA > 1000)  numA = numA / 10;
+    Console.WriteLine($"Третья цифра справа равна {numA % 10}");
+};
