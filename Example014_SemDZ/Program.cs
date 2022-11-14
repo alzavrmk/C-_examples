ff09569b5d40ff6c82993ebd8cc77a5d5a17b722
Anastasia <3845303@gmail.com>
2022-11-15 06:28:09 +0900

    Задача 10. Вывести вторую цифру трехзначного числа

---
+++ Example014_SemDZ/Program.cs
@@ -0,0 +1,5 @@
+﻿//Вывести вторую цифру трехзначного числа
+Console.Write("Введите трехзначное целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int b=(numberA / 10) % 10;
+Console.WriteLine($"Вторая цифра числа {numberA} равна {b}");
