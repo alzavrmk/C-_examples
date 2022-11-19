28ce79755c8767b2cc785a93676b44e4794f26f6
Anastasia <3845303@gmail.com>
2022-11-19 23:16:44 +0900

    Расстояние АВ в трехмерном пространстве

---
+++ Example018_DIST3D/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] x = new int[3];
+int[] y = new int[3];
+
+for (int i = 0; i < 3; i++)
+{
+    // Console.Write($"x{i+1}= ");  x[i]= Convert.ToInt32(Console.Read());  
+    // Console.Write($" y{i+1}= ");  y[i]= Convert.ToInt32(Console.ReadLine());  
+    x[i] = new Random().Next(1, 100);
+    y[i] = new Random().Next(1, 10);
+
+}
+Console.WriteLine($"A({x[0]}; {x[1]}; {x[2]})");
+Console.WriteLine($"B({y[0]}; {y[1]}; {y[2]})");
+
+double ab = Math.Sqrt(Math.Pow((x[0] - y[0]), 2) + Math.Pow((x[1] - y[1]), 2) + Math.Pow((x[2] - y[2]), 2));
+Console.WriteLine($"|AB|= {Math.Round(ab)}");
+
