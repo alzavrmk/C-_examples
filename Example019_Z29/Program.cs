d01eeb6526c0d9d6d5ee308818241407ed8df0cc
Anastasia <3845303@gmail.com>
2022-11-20 23:48:19 +0900

    Заполнение и вывод массива

---
+++ Example019_Z29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(0, 100);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите размерность массива");
+int n = Convert.ToInt16(Console.ReadLine());
+
+int[] numbers = new int[n];
+
+FillArray(numbers);
+PrintArray(numbers);
