1cd8799cbffc60f9a00bb7df1a7926c9d94ae4ab
Anastasia <3845303@gmail.com>
2022-11-20 23:17:26 +0900

    Функция считает сумму цифр целого числа

---
+++ Example019_Z27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int SummaDigitNumber(int number)
+{
+    int sum = 0;
+    int dig = 0;
+    while (number > 0)
+    {
+        dig = number % 10;
+        sum = sum + dig;
+        number = number / 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SummaDigitNumber(a));
